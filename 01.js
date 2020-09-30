5ffe88696a142568493f8f721acf648b775d35ff
Mikael Olsson <mikael.olsson@emmio.se>
2020-09-30 15:06:23 +0200

    Added / removed correct.php and test question

---
+++ 01.js
@@ -0,0 +1,9 @@
+/*
+
+Gör en for-loop som skriver ut 1-10, en rad i taget.
+
+*/
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
--- correct.php
+++ correct.php
@@ -2,6 +2,25 @@
 
 $path = dirname(__FILE__);
 
-$ret = exec("node '$path/01.js'", $out, $err);
+$file = "$path/01.js";
 
-echo 'kalle' == $ret;
+$ret = exec("node '$file'", $out, $err);
+
+echo 'Fråga 1:' . PHP_EOL;
+echo 'Resultat: ';
+
+if ( '1,2,3,4,5,6,7,8,9,10' === implode( ',', $out ) ) {
+	echo 'Korrekt';
+} else {
+	echo 'Ej korrekt';
+}
+echo PHP_EOL;
+
+echo 'Använt for-loop: ';
+if ( strpos( file_get_contents( $file ), 'for' ) !== false ) {
+	echo 'Korrekt';
+} else {
+	echo 'Ej korrekt';
+}
+echo PHP_EOL;
+
